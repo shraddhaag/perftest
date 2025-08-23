7eae5fa293749a237377b640f025f04ba1de7d74
Shraddha Agrawal <shraddha.agrawal000@gmail.com>
2025-08-23 18:45:42 +0530

    probe: loadtest the given endpoint
    This commit does the following:
    1. Adds two arguments to the CLI:
    	- duration - for which the loadtest should run for.
    	- concurrency - how many requests should be issued concurrently.
    2. Makes use of worker pool to issue as many requests as possible
    to the given endpoint for the duration specified.
    
    Signed-off-by: Shraddha Agrawal <shraddha.agrawal000@gmail.com>

--- bin/index.js
+++ bin/index.js
@@ -3,10 +3,18 @@
 const probe = require('./probe.js')
 const yargs = require('yargs');
 
-const argv = yargs(process.argv.slice(1))
-	.usage('Usage: $0 --endpoint [url]')
+const argv = yargs(process.argv.slice(2))
+	.usage('Usage: $0 --endpoint [url] --duration [dur] --concurrency [con]')
 	.demandOption(['endpoint'])
 	.parse(); 
 
-// console.log("Hello World!");
-probe.sendRequest(argv.endpoint)
+if (typeof argv.duration === 'undefined') {
+    argv.duration = 11;
+    console.log("Duration not specified. Falling back to default value of 11s.")
+}
+if (typeof argv.concurrency === 'undefined') {
+    argv.concurrency = 100;
+    console.log("Concurrency not specified. Falling back to default value of 100.")
+}
+
+probe.loadTestEnpoint(argv.endpoint, argv.duration, argv.concurrency)
--- bin/probe.js
+++ bin/probe.js
@@ -1,4 +1,12 @@
 const url = require('url');
+const { performance } = require('perf_hooks');
+
+const metrics = {
+    totalCount: 0, 
+    successCount: 0, 
+    failureCount: 0, 
+    totalBytes: 0, 
+}
 
 function sendRequest(urlStr) {
     var parsedUrl = url.parse(urlStr, true);
@@ -8,20 +16,53 @@ function sendRequest(urlStr) {
         host: parsedUrl.hostname,
         port: parsedUrl.port,
     };
-    const request = protocol.get(options, res => {
-        let bytes = 0; 
-        res.on("data", chunk => { bytes += chunk.length; }); 
-        res.on("end", () => {
-            console.log("status: ", res.statusCode); 
-            console.log("bytes: ", bytes); 
-        }); 
-    });
 
-    request.on("error", err => {
-        console.log("encountered an error ", err.message);
-    });
+    return new Promise((resolve, reject) => {
+        const request = protocol.get(options, res => {
+            let bytes = 0; 
+            res.on("data", chunk => { bytes += chunk.length; }); 
+            res.on("end", () => {
+                resolve({
+                    status: res.statusCode, 
+                    bytes: bytes, 
+                });
+            }); 
+        });
 
-    request.end(); 
+        request.on("error", reject); 
+
+        request.end(); 
+    });
 }
 
-module.exports = {sendRequest}; 
+async function worker(urlStr, deadline) {
+    while (performance.now() < deadline) {
+        metrics.totalCount++;
+
+        try {
+            const {status, bytes} = await sendRequest(urlStr);
+            if (status >= 200 && status <= 300) metrics.successCount++; else metrics.failureCount++;
+        } catch (error) {
+            metrics.failureCount++;
+        }
+    }
+}
+
+async function loadTestEnpoint(urlStr, duration, concurrency) {
+    durationInMilliSeconds = Math.max(1, Math.round(Number(duration) * 1000));
+    const startTime = performance.now(); 
+    const deadline = startTime + durationInMilliSeconds;  
+
+    const workers = Array(concurrency).fill().map(() => worker(urlStr, deadline));
+    
+    await Promise.all(workers); 
+
+    const endTime = performance.now(); 
+
+    console.log("Time elapsed: ", (endTime - startTime)/1000, "s");
+    console.log("Total Requests: ", metrics.totalCount);
+    console.log("Successful Requests: ", metrics.successCount);
+    console.log("Failed Requests: ", metrics.failureCount); 
+}
+
+module.exports = {loadTestEnpoint}; 
